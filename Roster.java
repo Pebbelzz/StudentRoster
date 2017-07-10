4b6567a49ebb2c6e8b8be30002efd93e680f776a
Josie Anderson <greeneyes14j@gmail.com>
2017-07-10 19:06:07 -0400

    Add files via upload

---
+++ Roster.java
@@ -0,0 +1,96 @@
+/**
+ * Created by Josie on 7/10/2017.
+ */
+import java.util.ArrayList;
+public class Roster {
+
+    private static ArrayList<Student> students = new ArrayList<>();
+
+    //adds Students
+    public static void add(String id, String firstNm, String lastNm, String eml, int age, int grade1, int grade2, int grade3){
+        int[] scores = {grade1, grade2, grade3};
+        Student newStudent = new Student(id, firstNm, lastNm, eml, age, scores);
+        students.add(newStudent);
+    }
+
+    //removes students from the roster by studentID
+    public static void remove(String studentID){
+
+    }
+
+    //prints a complete tab-separated list of student data using accessor methods
+    public static void print_all(){
+        for(int i=0; i < students.size(); i++){
+            students.get(i).print();
+        }
+    }
+
+    //prints a student's average grade by studentID
+    public static void print_average_grade(String studentID){
+        int indexNeeded = 0;
+        //finds the index of the student in
+        // students ArrayList that matches the studentID given
+        for(int i = 0; i < students.size(); i++){
+            if(students.get(i).getStudentID().equals(studentID)){
+                indexNeeded = i;
+            }
+        }
+        //retrieves the array of grades for the student
+        int[] arrForAvg = students.get(indexNeeded).getGrades();
+        int sum = 0;
+        //averages the grades in the given array
+        for(int j = 0; j < arrForAvg.length; j++){
+            sum = sum + arrForAvg[j];
+        }
+        System.out.println("Average grades for StudentID# " + studentID + " is " + sum/3);
+    }
+
+    //verifies student email addresses and displays all invalid e-mail addresses
+    //invalid email should include an @ sign and a period,
+    // and doesn't include a space
+    public static void print_invalid_emails(){
+        String emailToCheck = "";
+        String badEmails = "";
+        for(int x = 0; x < students.size(); x++){
+            emailToCheck = students.get(x).getEmail();
+
+            if(emailToCheck.indexOf(" ") != -1){
+                badEmails = badEmails + " " + emailToCheck;
+            }
+            else if(emailToCheck.indexOf("@") == -1){
+                badEmails = badEmails + " " + emailToCheck;
+            }
+            else if(emailToCheck.indexOf("@") > emailToCheck.indexOf(".", emailToCheck.indexOf("@"))) {
+                badEmails = badEmails + " " + emailToCheck;
+            }
+        }
+        System.out.println("The following emails are bad: " + badEmails);
+    }
+
+
+
+    public static void main (String[] args){
+        add( "1", "John", "Smith", "John1989@gmail.com", 20, 89,79, 59);
+        add("2", "Suzan", "Erickson", "Erickson_1990@gmail.com", 19, 91, 82, 85);
+        add("3", "Jack", "Napoli", "The_lawyer99yahoo.com", 19, 85, 84, 87);
+        add("4", "Erin", "Black", "Erin.black@comcast.net", 22, 91, 98, 82);
+        add("5", "Josie", "Anderson", "jand311@wgu.edu", 35, 90, 85, 93);
+
+
+        print_all();
+        print_invalid_emails();
+
+        //loop through ArrayList and for each element print average grade:
+        for(Student person : students){
+            print_average_grade(person.getStudentID());
+        }
+
+
+
+
+        remove("3");
+        remove("3");
+        //expected: this should print a message saying such a studen
+        //with this ID was not found
+    }
+}
---
+++ Student.java
@@ -0,0 +1,66 @@
+/**
+ * Created by Josie on 7/10/2017.
+ */
+public class Student {
+
+    //instance variables
+    private String studentID;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private int age;
+    private int[] grades = new int[3];
+
+    //Accessor Methods(getters)
+
+    //returns studentID
+    public String getStudentID(){return studentID;}
+    //returns firstName
+    public String getFirstName(){return firstName;}
+    //returns lastName
+    public String getLastName(){return lastName;}
+    //returns email
+    public String getEmail(){return email;}
+    //returns age
+    public int getAge(){return age;}
+    //returns grades
+    public int[] getGrades(){return grades;}
+
+
+    //Mutator Methods(setters)
+
+    //sets studentID
+    public void setStudentID(String id){this.studentID = id;}
+    //sets firstName
+    public void setFirstName(String fName){this.firstName = fName;}
+    //sets lastName
+    public void setLastName(String lName){this.lastName = lName;}
+    //sets email
+    public void setEmail(String emailAddress){this.email = emailAddress;}
+    //sets age
+    public void setAge(int howOld){this.age = howOld;}
+    //sets grades
+    public void setGrades(int[] scores){this.grades = scores;}
+
+
+    //constructor for Student
+    public Student(String id, String fName, String lName, String emailAddress, int howOld, int[]scores){
+        setStudentID(id);
+        setFirstName(fName);
+        setLastName(lName);
+        setEmail(emailAddress);
+        setAge(howOld);
+        setGrades(scores);
+    }
+
+    //prints Student data using getter methods
+    public void print(){
+        System.out.println(
+                "Student ID: " + getStudentID() +
+                        " | First Name: " + getFirstName() +
+                        " | Last Name: " + getLastName() +
+                        " | Email Address: " + getEmail() +
+                        " | Age: " + getAge()
+        );
+    }
+}
